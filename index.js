f33e65a4438912078fb7caace585d9641fa2f50c
Morgan Cividanes <morgancivida@gmail.com>
2020-11-27 18:24:41 -0800

    add handlebars for template

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+//this is basically importing express ******
 const express = require('express');
+const express_handle = require('express-handlebars');
+
 const app = express();
+app.engine('handlebars', express_handle());
+app.set('view engine', 'handlebars');
+//*********************************************
+
+//variable storing the port
 const port = 8080;
 
 //1st aguement: path, 2nd arguement: function
 app.get("/", (req, res) => {
-    res.send("hello world");
+    //req = request, res = response
+    //response sending hello world 
+    //res.send("<h1>hello world!</h1>");
+    res.render("home", {
+        name: "morgan"
+    });
 });
 
 app.listen(port, () => {
-    console.log('listening on http://localhost:${8080}');
+    //this is a debug to see in the console
+    console.log('listening on http://localhost:${port}');
 });
